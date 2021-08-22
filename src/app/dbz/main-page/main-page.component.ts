ea82a3482b1a50afab95ab7f56cf177da371d06d
GCastro68 <gcastro2805@gmail.com>
2021-08-22 12:28:13 -0600

    Servicios V1

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,28 +1,15 @@
 import { Component } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interfaces';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
-  personajes: Personaje[] = [
-    {
-      nombre: 'Goku',
-      poder: 15000,
-    },
-    {
-      nombre: 'Vegeta',
-      poder: 7500,
-    },
-  ];
-
   nuevo: Personaje = {
     nombre: 'Maestro Roshi',
     poder: 1000,
   };
-
-  agregarNuevoPersonaje(argumento: Personaje) {
-    this.personajes.push(argumento);
-  }
+  constructor() {}
 }
